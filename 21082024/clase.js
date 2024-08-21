96aacf0576b6bad516666e6006a25d199e4da664
Julian3295 <165791464+Julian3295@users.noreply.github.com>
2024-08-21 07:29:18 -0500

    practicas js.2

---
+++ 21082024/clase.js
@@ -0,0 +1,20 @@
+const a = 4 
+const b = 5
+const c = 1
+const l = true
+
+const x = b*a-b**2/4*c
+console.log("el resultado "+x)
+console.log ("ejercicio 2")
+const y = a*b/3**2
+console.log("el resultado es"+y)
+
+console.log ("ejercicio 3")
+
+const z = (((b+c)/2*a+10)*3*b)-6
+console.log("El resultado "+z)
+
+console.log ("ejercicio 4")
+const p = (a>b) && (!l) || (x**3 == 30)
+console.log("el resultado "+p)
+
---
+++ 21082024/practicas.js
@@ -0,0 +1,5 @@
+let nro1 = prompt("por favor digite numero 1")
+let nro2 = prompt("por favor numero 2")
+let nro3 = parent("por favor digite numero 3")
+let suma = nro1 + nro2 
+console.log("$ {nro1} + ${nro2} = ${suma}")
