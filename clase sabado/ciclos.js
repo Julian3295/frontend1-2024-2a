82bd6c1bb636773de681944ec6506af834e58be0
xiiimes <165798160+xiiimes@users.noreply.github.com>
2024-08-24 02:52:28 -0500

    clase sabado

---
+++ clase sabado/ciclos.js
@@ -0,0 +1,7 @@
+let numero = 10 
+
+while(numero > 0){
+    numero = numero - 1
+    console.log (numero)
+}
+ console.log("DESPEGAR")
---
+++ clase sabado/ejecicio.js
@@ -0,0 +1,6 @@
+var numero_Uno = prompt("digite numero uno")
+var numero_Dos = prompt("digite numero dos")
+
+var suma = parseFloat(numero_Uno) + parseFloat(numero_Dos)
+
+alert("el resultado es " + suma)
